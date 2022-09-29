a90e45b976fb0759698ab0ff3e8b12806f325ee2
Roberts <roberts.rozkalns@citadaskola.lv>
2022-09-29 15:16:09 +0300

    Car class, some methods

--- public/index.php
+++ public/index.php
@@ -2,4 +2,20 @@
 
 declare(strict_types=1);
 
+require_once '../src/Car.php';
+
+
+// Homework: Driver (name, surname, age)
+
+$car = (new Car(1000, 'AB-1234', 14.6))
+    ->addKilometers(200)
+    ->addKilometers(500);
+
+$liters = $car->getConsumedLiters();
+
+$car = null;
+
+var_dump($liters);
+
+
 //require __DIR__ . '/../bootstrap/app.php';
