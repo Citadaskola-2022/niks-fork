ebce9ebcdfffc817efaf3c58025149444972d173
Niks <niksklava@gmail.com>
2022-10-06 14:58:41 +0300

    problem

---
+++ src/car.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+class Car
+{
+    private ?int $odometer;
+    private string $number_plate;
+    private float $fuelConsumption;
+
+    public function __construct(
+        int $odometer,
+        string $number_plate,
+        private readonly float $fuelConsumption
+    )
+    {
+        $this->odometer = $odometer;
+        $this->number_plate = $number_plate;
+    }
+
+    public function addKilometers(int $kilometer): Car
+    {
+        $this->odometer += $kilometer;
+
+        return $this;
+    }
+
+    public function getConsumedLiters(): float
+    {
+        return $this->odometer * $this->fuelConsumption / 100;
+    }
+
+    public function __destruct()
+    {
+        // echo "Car $this->number_plate Destroyed </br>" . PHP_EOL;
+    }
+}
