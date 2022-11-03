baeeb23feabca48cdccbe9100d3a5dcbe8e4a01c
Niks <niksklava@gmail.com>
2022-11-03 15:21:07 +0200

    3rd party packages, env variable

--- public/index.php
+++ public/index.php
@@ -4,7 +4,32 @@ declare(strict_types=1);
 
 require __DIR__ . '/../bootstrap/app.php';
 
+use Ramsey\Uuid\Uuid;
 
-$transaction = new \App\ServiceCosts(200);
-$transaction->process();
+//$transaction = new \App\ServiceCosts(200);
+//$transaction->process();
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+//echo '<pre>';
+var_dump($_ENV['DB_USER']);
+var_dump($_ENV['DB_PASSWORD']);
+
+//$inflector = InflectorFactory::create()->build();
+//    $name = 'apple';
+//printf(
+//   'vienskaitlis: %s, daudzskaitlis: %s',
+//    $name, $inflector->pluralize($name)
+//);
+//echo '<br>';
+
+
+$uuid = Uuid::uuid4();
+
+printf(
+    "UUID: %s\nVersion: %d\n",
+    $uuid->toString(),
+    $uuid->getFields()->getVersion()
+);
 
